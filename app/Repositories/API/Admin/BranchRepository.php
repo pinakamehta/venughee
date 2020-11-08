bb1651fcd1688c3a6db1d88b53d61722020def01
Pinak Mehta <pinakamehta@gmail.com>
2020-11-08 20:06:33 +0530

    change mail logic

--- app/Repositories/API/Admin/BranchRepository.php
+++ app/Repositories/API/Admin/BranchRepository.php
@@ -64,9 +64,7 @@ class BranchRepository
 
         $login_mail_data = ['username' => $data['branch_contact_number'], 'password' => $auto_generated_password];
 
-        Mail::send('emails.send_login_detail', [
-            $login_mail_data
-        ], function ($message) use ($data) {
+        Mail::send('emails.send_login_detail', $login_mail_data, function ($message) use ($data) {
             $message->to($data['branch_email'])->subject('Login Details.!');
         });
         return;
